e17096136f1b5c925691a2c84e91f279214e9fab
begimov <begimov@gmail.com>
2018-02-22 16:15:09 +0300

    static macro method to add new methods added to Macroable trait

--- app/Support/Macroable.php
+++ app/Support/Macroable.php
@@ -7,5 +7,10 @@ namespace App\Support;
  */
 trait Macroable
 {
-    //
+    protected static $macros = [];
+
+    public static function macro($name, $macro)
+    {
+        static::$macros[$name] = $macro;
+    }
 }
