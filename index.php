3109c0b763c8c0f01f42639177d20bb049d202c0
begimov <begimov@gmail.com>
2018-02-22 16:10:32 +0300

    custom method added to Request via macro method of Macroable trait

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
+use App\Http\Request;
+
 require_once 'vendor/autoload.php';
+
+Request::macro('getMethod', function () {
+    return $this->method;
+});
+
+$request = new Request();
+
+dump($request->getMethod());
